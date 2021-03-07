20e0f13fbdc23c30738dbef395f3f50930b8d854
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-07 22:13:16 +0100

    Add DisposalUnitAmogusWindow

---
+++ Content.Client/GameObjects/Components/Disposal/DisposalUnitAmogusWindow.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+using System;
+using Content.Client.Utility;
+using Robust.Client.ResourceManagement;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.CustomControls;
+using Robust.Shared.IoC;
+
+namespace Content.Client.GameObjects.Components.Disposal
+{
+    public class DisposalUnitAmogusWindow : SS14Window
+    {
+        private TextureRect Image { get; }
+
+        public Action? OnEngaged;
+
+        public DisposalUnitAmogusWindow()
+        {
+            AddChild(new VBoxContainer
+            {
+                Children =
+                {
+                    (Image = new TextureRect
+                    {
+                        Texture = IoCManager.Resolve<IResourceCache>()
+                            .GetTexture("/Textures/Constructible/disposalup.png"),
+                        MouseFilter = MouseFilterMode.Stop,
+                    })
+                }
+            });
+
+            Title = "Disposal Unit";
+            Image.OnKeyBindDown += ImagePressed;
+        }
+
+        private void ImagePressed(GUIBoundKeyEventArgs obj)
+        {
+            Image.Texture = IoCManager.Resolve<IResourceCache>().GetTexture("/Textures/Constructible/disposaldown.png");
+
+            OnEngaged?.Invoke();
+        }
+    }
+}
--- Content.Client/GameObjects/Components/Disposal/DisposalUnitBoundUserInterface.cs
+++ Content.Client/GameObjects/Components/Disposal/DisposalUnitBoundUserInterface.cs
@@ -12,7 +12,7 @@ namespace Content.Client.GameObjects.Components.Disposal
     [UsedImplicitly]
     public class DisposalUnitBoundUserInterface : BoundUserInterface
     {
-        private DisposalUnitWindow? _window;
+        private DisposalUnitAmogusWindow? _window;
 
         public DisposalUnitBoundUserInterface(ClientUserInterfaceComponent owner, object uiKey) : base(owner, uiKey)
         {
@@ -27,14 +27,15 @@ namespace Content.Client.GameObjects.Components.Disposal
         {
             base.Open();
 
-            _window = new DisposalUnitWindow();
+
+            _window = new DisposalUnitAmogusWindow();
 
             _window.OpenCentered();
             _window.OnClose += Close;
 
-            _window.Eject.OnPressed += _ => ButtonPressed(UiButton.Eject);
-            _window.Engage.OnPressed += _ => ButtonPressed(UiButton.Engage);
-            _window.Power.OnPressed += _ => ButtonPressed(UiButton.Power);
+            // _window.Eject.OnPressed += _ => ButtonPressed(UiButton.Eject);
+            _window.OnEngaged += () => ButtonPressed(UiButton.Engage);
+            // _window.Power.OnPressed += _ => ButtonPressed(UiButton.Power);
         }
 
         protected override void UpdateState(BoundUserInterfaceState state)
@@ -46,7 +47,7 @@ namespace Content.Client.GameObjects.Components.Disposal
                 return;
             }
 
-            _window?.UpdateState(cast);
+            // _window?.UpdateState(cast);
         }
 
         protected override void Dispose(bool disposing)
